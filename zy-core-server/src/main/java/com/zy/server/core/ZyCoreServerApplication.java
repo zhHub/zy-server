8d22412fb449193092f9b32cf0d477ded4f84213
幼年星辰 <18143774515@163.com>
2021-05-24 21:45:20 +0800

    init: 初始化core

---
+++ zy-core-server/src/main/java/com/zy/server/core/ZyCoreServerApplication.java
@@ -0,0 +1,21 @@
+package com.zy.server.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * fileName: ZyCoreServerApplication
+ * create: 2021-5-24 21:33
+ * description:
+ * history:
+ *
+ * @author 张建元 18143774515@163.com
+ */
+@SpringBootApplication
+public class ZyCoreServerApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(ZyCoreServerApplication.class, args);
+    }
+    
+}
